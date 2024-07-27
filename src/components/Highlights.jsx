9ccdf67b730145b68eec21dbf5753d0c7a7dad3d
Avi <avimahalingpure10@gmail.com>
2024-07-27 15:25:40 +0530

    added videos for highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import {rightImg, watchImg} from '../utils'
+import VideoCarousel from './VideoCarousel'
 
 const Highlights = () => {
 
@@ -10,14 +12,28 @@ const Highlights = () => {
       y: 0,
       delay: 1
     })
+
+    gsap.to('.link', {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    })
   },[])
 
   return (
     <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
       <div className="screen-max-width">
-        <div className='mb-12 w-full items-end justify-between'>
+        <div className='mb-12 w-full md:flex items-end justify-between'>
           <h1 id='title' className='section-heading'> Get the highlights.</h1>
+          <div className='flex flex-wrap items-end gap-5'>
+            <p className='link'>watch the film <img src={watchImg} alt="" className='ml-2' /></p>
+            <p className='link'>watch the event <img src={rightImg} alt="" className='ml-2' /></p>
+          </div>
         </div>
+
+      <VideoCarousel/>
+
       </div>
     </section>
   )
