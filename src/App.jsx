83b13bf1b0136b74195401506290c5e3c53b0cfd
Avi <avimahalingpure10@gmail.com>
2024-07-27 23:27:57 +0530

    adding 3d iphone model

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Highlights from './components/Highlights.jsx'
+import Model from './components/Model.jsx'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Highlights/>
+      <Model/>
     </main>
   )
 }
---
+++ src/components/ModelView.jsx
@@ -0,0 +1,12 @@
+import { View } from '@react-three/drei'
+import React from 'react'
+
+const ModelView = ({index,groupRef,gsapType,controlRef,setRotationState,item,size}) => {
+  return (
+    <View index={index} id={gsapType} className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}>
+        
+    </View>
+  )
+}
+
+export default ModelView
